2980d448a15f0db9eda150ee87db91c911e21f0c
Tom Creuse <tom.creuse@gmail.com>
2024-02-26 21:36:42 +0100

    modified:assets/scripts.min.js

--- assets/scripts.min.js
+++ assets/scripts.min.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   $(".gallery").mauGallery({
     columns: { xs: 1, sm: 2, md: 3, lg: 3, xl: 3 },
-    lightBox: !0,
+    lightBox: true,
     lightboxId: "myAwesomeLightbox",
-    showTags: !0,
+    showTags: true,
     tagsPosition: "top",
   });
 });
