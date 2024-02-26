42ca815d24455668ba396ff19e2478b966fd23e5
Tom Creuse <tom.creuse@gmail.com>
2024-02-26 09:25:43 +0100

    modification des photos mobile

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+$(document).ready(function () {
+  $(".gallery").mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
+  // écrire le code ici : selectionner les div avec les images et définir leur src (penser à les enlever du html)
 });
